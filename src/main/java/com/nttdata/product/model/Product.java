4aa3f2b7139eecf32ffcf427ddaaed135dfcc251
Eduardo Orrego <corregoa@everis.com>
2024-03-17 22:22:40 -0500

    Refactoring Entity Collection - Functional Requeriments

--- src/main/java/com/nttdata/product/model/Product.java
+++ src/main/java/com/nttdata/product/model/Product.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -16,6 +17,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "product")
 public class Product {
 
+    @Id
     private String id;
     private String type;
     private String currency;
